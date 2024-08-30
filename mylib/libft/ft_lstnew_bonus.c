5716cbf38ceade35ff8608d72fce014d598e1791
HUSSEINKHRAYZAT <97231643+HUSSEINKHRAYZAT@users.noreply.github.com>
2024-08-30 20:06:39 +0300

    Add files via upload

---
+++ mylib/libft/ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkhrayza <hkhrayza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 23:05:49 by hkhrayza          #+#    #+#             */
+/*   Updated: 2024/08/29 23:05:49 by hkhrayza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (! new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
